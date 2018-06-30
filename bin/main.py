46d56df031d786d110796fa5cf02f2e65047ee44
BadBoySeventeen <res.badboyseventeen@gmail.com>
2018-06-30 12:18:22 +0800

    添加新命令, 使用正则分析url

--- bin/main.py
+++ bin/main.py
@@ -5,6 +5,7 @@ import telebot
 import base64
 import sys
 import time
+import re
 from config import token
 bot = telebot.TeleBot(token)
 
@@ -19,7 +20,7 @@ def send_welcome(message):
 
 def return_arg(str):
     'return arg from command'
-    return str.split()[1]
+    return str.split(" ", 1)[1]
 
 @bot.message_handler(commands=['b64encode'])
 def base64_encode(message):
@@ -36,6 +37,17 @@ def base64_decode(message):
         splited = return_arg(message.text)
         handled = base64.b64decode(splited)
         bot.reply_to(message, handled)
+    except:
+        bot.reply_to(message, '引发错误! 请确认自己没有输入错误的参数')
+
+@bot.message_handler(commands=['cloudmusic'])
+def findall_cloudmusic_id(message):
+    try:
+        splited = return_arg(message.text)
+        handled = re.findall('\d+', re.search('song.*userid?', splited).group())[0]
+        copyable = '`%s`' % handled
+        bot.reply_to(message, copyable, parse_mode="Markdown")
+        #bot.send_message(message.chat.id, handled, parse_mode="Markdown")
     except:
         bot.reply_to(message, '引发错误! 请确认自己没有输入错误的参数')
 
